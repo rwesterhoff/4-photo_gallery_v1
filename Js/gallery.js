8d98e564a599a46426dd70ec23cbc4cbcd70b4eb
Robin Westerhoff <rwesterhoff@me.com>
2016-06-08 22:09:14 +0200

    Structurizing code

--- Js/gallery.js
+++ Js/gallery.js
@@ -70,26 +70,6 @@ function highlightSelected(selectItem) {
     selectItem.addClass($classSelected).siblings().removeClass($classSelected);
 }
 
-//Get overlay ready to show
-function injectOverlay() {
-    $('body').prepend($overlayHtml);
-    $($overlay).hide();
-    $($overlay).append($closeHtml + $carouselHtml);
-    $($carousel).append($slidesWrapperHtml);
-    $($slidesWrapper).append($slideHtml + $prevButtonHtml + $nextButtonHtml);
-}
-
-//Remove it again
-function removeOverlay() {
-    $($overlay).remove();
-}
-
-//Get data from selected item (getting stuff)
-function getSlideData(selectItem) {
-    $activeLink = selectItem.find('a').attr('href');
-    $activeCaption = selectItem.find('img').attr('title');
-}
-
 function showArrows() {
     $($prevButton).show();
     $($nextButton).show();
@@ -105,8 +85,11 @@ function hideNextArrow() {
 
 //Load data in slide (setting stuff)
 function loadCarouselSlide() {
-    //Empty and load fresh data
+
+    //Empty the old slide and load fresh data
     $($currentSlide).empty();
+
+    //Set img or youtube iframe as slide content
     if ($activeLink.startsWith('https://youtube.com/')) {
         $($currentSlide).append($iframeHtml);
         $($currentSlide).find('iframe').attr('src', $activeLink);
@@ -114,10 +97,12 @@ function loadCarouselSlide() {
         $($currentSlide).append($imageHtml);
         $($currentSlide).find('img').attr('src', $activeLink);
     }
+
+    //Add the caption
     $($currentSlide).append($captionHtml);
     $($currentSlide).find('figcaption').text($activeCaption);
 
-    //Check the items index
+    //Check the items index and load the controls
     if ($currentItemIndex === $firstItemIndex) {
         hidePrevArrow();
     } else if ($currentItemIndex === $lastItemIndex) {
@@ -125,15 +110,12 @@ function loadCarouselSlide() {
     } else {
         showArrows();
     }
+
 }
 
-//Animate slide
-function animateSlide() {
-    $($currentSlide).fadeOut($singleDuration).fadeIn($singleDuration);
-}
 
 //Check the index (and log it for testing and debugging)
-function checkIndex() {
+function setIndex() {
     $prevItemIndex = $currentItemIndex - 1;
     $nextItemIndex = $currentItemIndex + 1;
     // console.log('currentItemIndex: ' + $currentItemIndex);
@@ -143,84 +125,114 @@ function checkIndex() {
     // console.log('lastItemIndex: ' + $lastItemIndex);
 }
 
-//Get new data en load slide
-function getNewSlide(selectItem) {
-    highlightSelected(selectItem);
-    getSlideData(selectItem);
-    animateSlide();
-    setTimeout(loadCarouselSlide, $singleDuration);
-    checkIndex();
-}
-
-function getPrevSlide() {
-    if ($currentItemIndex > $firstItemIndex) {
-        showArrows();
-        $currentItemIndex -= 1;
-        getNewSlide($($selectedItem).prev());
-    }
-    if ($currentItemIndex === $firstItemIndex) {
-        hidePrevArrow();
-    }
-}
-
-function getNextSlide() {
-    if ($currentItemIndex < $lastItemIndex) {
-        showArrows();
-        $currentItemIndex += 1;
-        getNewSlide($($selectedItem).next());
-    }
-    if ($currentItemIndex === $lastItemIndex) {
-        hideNextArrow();
-    }
-}
 
 
-//Control the entire thing
-function carouselControl() {
 
-    //Hiding the overlay
-    function hideOverlay() {
-        $($overlay).fadeOut($doubleDuration);
-        setTimeout(removeOverlay, $doubleDuration);
-        $(document).off('keydown');
-    }
-
-    //On click left + right arrows
-    $($prevButton).click(function() {
-        getPrevSlide();
-    });
-    $($nextButton).click(function() {
-        getNextSlide();
-    });
-    $($closeButton).click(function() {
-        hideOverlay();
-    });
-
-    //On keypress
-    $(document).on('keydown', function(event) {
-        switch (event.which) {
-            case 37: // Left arrow
-                getPrevSlide();
-                break;
-
-            case 39: // Right arrow
-                getNextSlide();
-                break;
-
-            case 27: // 'Esc'
-                hideOverlay();
-                break;
-
-            default:
-                return; // exit this handler for other keys
-
-        }
-    });
-}
 
 //Get and set all data for the carousel 
 //based on the click event
 function showOverlay(selectItem) {
+
+    function injectOverlay() {
+        $('body').prepend($overlayHtml);
+        $($overlay).hide();
+        $($overlay).append($closeHtml + $carouselHtml);
+        $($carousel).append($slidesWrapperHtml);
+        $($slidesWrapper).append($slideHtml + $prevButtonHtml + $nextButtonHtml);
+    }
+
+    //Get data from selected item (getting stuff)
+    function getSlideData(selectItem) {
+        $activeLink = selectItem.find('a').attr('href');
+        $activeCaption = selectItem.find('img').attr('title');
+    }
+
+    //Control the entire thing
+    function carouselControl() {
+
+
+        //Get new data en load slide
+        function getNewSlide(selectItem) {
+
+            function animateSlide() {
+                $($currentSlide).fadeOut($singleDuration).fadeIn($singleDuration);
+            }
+
+            highlightSelected(selectItem);
+            getSlideData(selectItem);
+            animateSlide();
+            setTimeout(loadCarouselSlide, $singleDuration);
+            setIndex();
+        }
+
+        function setPrevSlide() {
+            if ($currentItemIndex > $firstItemIndex) {
+                showArrows();
+                $currentItemIndex -= 1;
+                getNewSlide($($selectedItem).prev());
+            }
+            if ($currentItemIndex === $firstItemIndex) {
+                hidePrevArrow();
+            }
+        }
+
+        function setNextSlide() {
+            if ($currentItemIndex < $lastItemIndex) {
+                showArrows();
+                $currentItemIndex += 1;
+                getNewSlide($($selectedItem).next());
+            }
+            if ($currentItemIndex === $lastItemIndex) {
+                hideNextArrow();
+            }
+        }
+
+        //Hiding the overlay
+        function hideOverlay() {
+            $($overlay).fadeOut($doubleDuration);
+
+            function removeOverlay() {
+                $($overlay).remove();
+            }
+
+            setTimeout(removeOverlay, $doubleDuration);
+            $(document).off('keydown');
+
+        }
+
+        //On click left + right arrows
+        $($prevButton).click(function() {
+            setPrevSlide();
+        });
+        $($nextButton).click(function() {
+            setNextSlide();
+        });
+        $($closeButton).click(function() {
+            hideOverlay();
+        });
+
+        //On keypress
+        $(document).on('keydown', function(event) {
+            switch (event.which) {
+                case 37: // Left arrow
+                    setPrevSlide();
+                    break;
+
+                case 39: // Right arrow
+                    setNextSlide();
+                    break;
+
+                case 27: // 'Esc'
+                    hideOverlay();
+                    break;
+
+                default:
+                    return; // exit this handler for other keys
+            }
+        });
+    }
+
+    injectOverlay();
     highlightSelected(selectItem);
     getSlideData(selectItem);
     loadCarouselSlide();
@@ -264,15 +276,13 @@ $($input).keyup(function() {
 
 //On click of thumbnail
 $($galleryItem).click(function(event) {
+
     //Define index of clicked item
     $currentItemIndex = $($(this)).index();
-    // alert($currentItemIndex);
-
-    //Prevent default interaction   
     event.preventDefault();
-    injectOverlay();
+
     showOverlay($(this));
-    checkIndex();
+    setIndex();
 });
 
 
@@ -281,13 +291,16 @@ $($galleryItem).mouseover(function() {
     $hoverTile = '<p class="js-alt-active">' + $(this).find('img').attr('alt') + '</p>';
     $(this).prepend($hoverTile);
 });
+
 $($galleryItem).mouseout(function() {
     removeFocusFx();
 });
+
 $($galleryItem + ' a').focus(function() {
     $hoverTile = '<p class="js-alt-active">' + $(this).find('img').attr('alt') + '</p>';
     $(this).prepend($hoverTile);
 });
+
 $($galleryItem + ' a').blur(function() {
     removeFocusFx();
 });
